6423103d953a0e04237519ea70204fd3a1662ade
Stefan Miljevic <stefi5miljevic@gmail.com>
2020-07-25 13:55:25 +0200

    Add UI library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+import { Button } from "@material-ui/core";
 
 const App = () => {
   return (
     <React.StrictMode>
-      <h1>Efforge Studio</h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>Efforge Studio</h1>
+        <p>Hello world from the application</p>
+        <Button variant="contained" color="primary">Default</Button>
+      </ThemeProvider>
     </React.StrictMode>
   );
 };
